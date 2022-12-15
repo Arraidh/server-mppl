92eafe6ac99382ae77c40e34b7760bd53cf6864e
Arraidh <ari.darmika@gmail.com>
2022-12-16 03:18:54 +0700

    make it better

---
+++ src/components/orders/OrderDetailmain.js
@@ -0,0 +1,114 @@
+import React, { useEffect } from "react";
+import OrderDetailProducts from "./OrderDetailProducts";
+import OrderDetailInfo from "./OrderDetailInfo";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deliveredOrder,
+  getOrderDetails,
+} from "../../Redux/Actions/OrderActions";
+import Loading from "../LoadingError/Loading";
+import Message from "../LoadingError/Error";
+import moment from "moment";
+
+const OrderDetailmain = (props) => {
+  const { orderId } = props;
+  const dispatch = useDispatch();
+
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { loading, error, orders } = orderDetails;
+
+  const orderDelivered = useSelector((state) => state.orderDelivered);
+  const { loading: loadingDelivered, success: successDelivered } =
+    orderDelivered;
+
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId));
+  }, [dispatch, orderId, successDelivered]);
+
+  const deliverHandler = () => {
+    dispatch(deliveredOrder(orders));
+  };
+  return (
+    <section className="content-main">
+      <div className="content-header">
+        <Link to="/orders" className="btn btn-dark text-white">
+          Back To Orders
+        </Link>
+      </div>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="alert-danger">{error}</Message>
+      ) : (
+        <div className="card">
+          <header className="card-header p-3 Header-green">
+            <div className="row align-items-center ">
+              <div className="col-lg-6 col-md-6">
+                <span>
+                  <i className="far fa-calendar-alt mx-2"></i>
+                  <b className="text-white">
+                    {moment(orders.createdAt).format("llll")}
+                  </b>
+                </span>
+                <br />
+                <small className="text-white mx-3 ">
+                  Order ID: {orders._id}
+                </small>
+              </div>
+              <div className="col-lg-6 col-md-6 ms-auto d-flex justify-content-end align-items-center">
+                <select
+                  className="form-select d-inline-block"
+                  style={{ maxWidth: "200px" }}
+                >
+                  <option>Change status</option>
+                  <option>Awaiting payment</option>
+                  <option>Confirmed</option>
+                  <option>Shipped</option>
+                  <option>Delivered</option>
+                </select>
+                <Link className="btn btn-success ms-2" to="#">
+                  <i className="fas fa-print"></i>
+                </Link>
+              </div>
+            </div>
+          </header>
+          <div className="card-body">
+            {/* Order info */}
+            <OrderDetailInfo order={orders} />
+
+            <div className="row">
+              <div className="col-lg-9">
+                <div className="table-responsive">
+                  <OrderDetailProducts order={orders} loading={loading} />
+                </div>
+              </div>
+              {/* Payment Info */}
+              <div className="col-lg-3">
+                <div className="box shadow-sm bg-light">
+                  {orders.isDelivered ? (
+                    <button className="btn btn-success col-12">
+                      DELIVERED AT {moment(orders.paidAt).format("MMM Do YY")}
+                    </button>
+                  ) : (
+                    <>
+                      {loadingDelivered && <Loading />}
+                      <button
+                        onClick={deliverHandler}
+                        className="btn btn-warning col-12"
+                      >
+                        MARK AS DELIVERED
+                      </button>
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default OrderDetailmain;
